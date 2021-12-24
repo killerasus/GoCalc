cb7880fc94f450a3711ed82bcd3eb1e3dc2168ef
Bruno Baère Pedrazzi Lomba de Araujo <killerasus@gmail.com>
2021-12-24 05:04:56 -0300

    Allows testing fixtures parallelization

--- calculator_test.go
+++ calculator_test.go
@@ -52,6 +52,7 @@ func TestUnaryNegativeExpectsPositive(t *testing.T) {
 }
 
 func TestUnaryNegativeExpectsZero(t *testing.T) {
+	t.Parallel()
 	var want float64 = 0
 	got := calculator.UnaryNegative(0)
 	if want != got {
@@ -60,6 +61,7 @@ func TestUnaryNegativeExpectsZero(t *testing.T) {
 }
 
 func TestDivideSame(t *testing.T) {
+	t.Parallel()
 	var want float64 = 1
 	got := calculator.Divide(4, 4)
 	if want != got {
@@ -68,6 +70,7 @@ func TestDivideSame(t *testing.T) {
 }
 
 func TestDivideLesserThan(t *testing.T) {
+	t.Parallel()
 	var want float64 = 0.5
 	got := calculator.Divide(1, 2)
 	if want != got {
@@ -76,6 +79,7 @@ func TestDivideLesserThan(t *testing.T) {
 }
 
 func TestDivideGreaterThan(t *testing.T) {
+	t.Parallel()
 	var want float64 = 3
 	got := calculator.Divide(9, 3)
 	if want != got {
@@ -84,6 +88,7 @@ func TestDivideGreaterThan(t *testing.T) {
 }
 
 func TestDivideByZero(t *testing.T) {
+	t.Parallel()
 	var want float64 = math.Inf(0)
 	got := calculator.Divide(1, 0)
 	if want != got {
