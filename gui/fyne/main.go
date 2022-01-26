02519a8f3b849581a3314ab09ee16632b67a31fc
Bruno Baère Pedrazzi Lomba de Araujo <killerasus@gmail.com>
2022-01-26 03:03:16 -0300

    Implements keyboard events. Closes #11

--- gui/fyne/main.go
+++ gui/fyne/main.go
@@ -37,7 +37,7 @@ func main() {
 		stack.SetText("")
 		for i := 0; i < calc.Size(); i++ {
 			v, _ := calc.Peeki(i)
-			stack.SetText(fmt.Sprint(stack.Text, fmt.Sprint(v), "\n"))
+			stack.SetText(fmt.Sprint(stack.Text, calcFormat(v), "\n"))
 		}
 	}
 
@@ -55,68 +55,119 @@ func main() {
 		}
 	}
 
+	zeroPressed := func() {
+		if v, err := strconv.ParseFloat(display.Text, 64); err != nil || v == 0.0 {
+			display.SetText("0")
+		} else {
+			addNumber(0)
+		}
+	}
+
+	enterPressed := func() {
+		v, err := strconv.ParseFloat(display.Text, 64)
+		if err == nil {
+			calc.Push(v)
+		}
+		display.SetText("0")
+		updateMemory()
+	}
+
+	addPressed := func() {
+		if _, ok := calc.Add(); !ok {
+			display.SetText("Error")
+		}
+		updateMemory()
+	}
+
+	subtractPressed := func() {
+		if _, ok := calc.Subtract(); !ok {
+			display.SetText("Error")
+		}
+		updateMemory()
+	}
+
+	multiplyPressed := func() {
+		if _, ok := calc.Multiplication(); !ok {
+			display.SetText("Error")
+		}
+		updateMemory()
+	}
+
+	dividePressed := func() {
+		if v, ok := calc.Division(); !ok {
+			if v == math.Inf(0) {
+				display.SetText("Division by zero")
+			} else {
+				display.SetText("Error")
+			}
+		}
+		updateMemory()
+	}
+
 	keypad := container.New(
 		layout.NewGridLayout(4),
 		widget.NewButton("7", func() { addNumber(7) }),
 		widget.NewButton("8", func() { addNumber(8) }),
 		widget.NewButton("9", func() { addNumber(9) }),
-		widget.NewButton("+", func() {
-			if _, ok := calc.Add(); !ok {
-				display.SetText("Error")
-			}
-			updateMemory()
-		}),
+		widget.NewButton("+", func() { addPressed() }),
 		widget.NewButton("4", func() { addNumber(4) }),
 		widget.NewButton("5", func() { addNumber(5) }),
 		widget.NewButton("6", func() { addNumber(6) }),
-		widget.NewButton("-", func() {
-			if _, ok := calc.Subtract(); !ok {
-				display.SetText("Error")
-			}
-			updateMemory()
-		}),
+		widget.NewButton("-", func() { subtractPressed() }),
 		widget.NewButton("1", func() { addNumber(1) }),
 		widget.NewButton("2", func() { addNumber(2) }),
 		widget.NewButton("3", func() { addNumber(3) }),
-		widget.NewButton("*", func() {
-			if _, ok := calc.Multiplication(); !ok {
-				display.SetText("Error")
-			}
-			updateMemory()
-		}),
-		widget.NewButton("0", func() {
-			if v, err := strconv.ParseFloat(display.Text, 64); err != nil || v == 0.0 {
-				display.SetText("0")
-			} else {
-				addNumber(0)
-			}
-		}),
+		widget.NewButton("*", func() { multiplyPressed() }),
+		widget.NewButton("0", func() { zeroPressed() }),
 		widget.NewButton(".", func() {
 			// TODO
 		}),
-		widget.NewButton("Enter", func() {
-			v, err := strconv.ParseFloat(display.Text, 64)
-			if err == nil {
-				calc.Push(v)
-			}
-			display.SetText("0")
-			updateMemory()
-		}),
-		widget.NewButton("/", func() {
-			if v, ok := calc.Division(); !ok {
-				if v == math.Inf(0) {
-					display.SetText("Division by zero")
-				} else {
-					display.SetText("Error")
-				}
-			}
-			updateMemory()
-		}),
+		widget.NewButton("Enter", func() { enterPressed() }),
+		widget.NewButton("/", func() { dividePressed() }),
 	)
 
 	input := container.NewVBox(display, keypad)
 	memoryBox := container.NewVBox(memory, stack)
 	box := container.NewHBox(memoryBox, input)
+
+	window.Canvas().SetOnTypedKey(func(ke *fyne.KeyEvent) {
+		switch ke.Name {
+		case fyne.Key1:
+			addNumber(1)
+		case fyne.Key2:
+			addNumber(2)
+		case fyne.Key3:
+			addNumber(3)
+		case fyne.Key4:
+			addNumber(4)
+		case fyne.Key5:
+			addNumber(5)
+		case fyne.Key6:
+			addNumber(6)
+		case fyne.Key7:
+			addNumber(7)
+		case fyne.Key8:
+			addNumber(8)
+		case fyne.Key9:
+			addNumber(9)
+		case fyne.Key0:
+			zeroPressed()
+		case fyne.KeyPlus:
+			addPressed()
+		case fyne.KeyMinus:
+			subtractPressed()
+		case fyne.KeyAsterisk:
+			multiplyPressed()
+		case fyne.KeySlash:
+			dividePressed()
+		case fyne.KeyEqual:
+			enterPressed()
+		case fyne.KeyReturn:
+			enterPressed()
+		default:
+
+		}
+	})
 
 	window.SetContent(box)
 
